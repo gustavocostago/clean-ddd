b2a3402583cb05d9c34eb85543cc525e10c11de9
Gustavo Costa <gustavocosta.cba@gmail.com>
2023-06-29 16:05:39 -0400

    fix: vitest

--- src/domain/entities/answer.ts
+++ src/domain/entities/answer.ts
@@ -1,10 +1,19 @@
 import { randomUUID } from 'crypto'
 
 export class Answer {
-  public id: string
-  public content: string
-  constructor(content: string, id?: string) {
-    this.content = content
-    this.id = id ?? randomUUID()
-  }
+	public id: string
+	public content: string
+	public authorId: string
+	public questionId: string
+	constructor(
+		content: string,
+		questionId: string,
+		authorId: string,
+		id?: string
+	) {
+		this.content = content
+		this.authorId = authorId
+		this.questionId = questionId
+		this.id = id ?? randomUUID()
+	}
 }
