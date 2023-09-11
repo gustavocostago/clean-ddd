cfd0a273bf31228c0db11f13f442da1091d216b6
Gustavo Costa <gustavocosta.cba@gmail.com>
2023-09-11 14:18:08 -0400

    refactor: answer and question

--- src/domain/entities/answer.ts
+++ src/domain/entities/answer.ts
@@ -1,9 +1,12 @@
 import { Entity } from '../../core/entities/entity'
+import { UniqueEntityID } from '../../core/entities/unique-entity-id'
 
 interface AnswerProps {
+  questionId: UniqueEntityID
+  authorId: UniqueEntityID
   content: string
-  questionId: string
-  authorId: string
+  createAt: Date
+  updateAt?: Date
 }
 export class Answer extends Entity<AnswerProps> {
   get content() {
