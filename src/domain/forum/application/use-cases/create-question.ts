213e366957b0c4d6f8625e539c7eef2b21ce4514
Gustavo Costa <gustavocosta.cba@gmail.com>
2023-11-26 10:31:49 -0400

    feat: attachments

--- src/domain/forum/application/use-cases/create-question.ts
+++ src/domain/forum/application/use-cases/create-question.ts
@@ -2,11 +2,13 @@ import { UniqueEntityID } from '@/core/entities/unique-entity-id'
 import { Question } from '../../enterprise/entities/question'
 import { QuestionsRepository } from '../repositories/questions-repository'
 import { Either, right } from '@/core/either'
+import { QuestionAttachment } from '../../enterprise/entities/question-attachment'
 
 interface CreateQuestionUseCaseRequest {
   authorId: string
   title: string
   content: string
+  attachmentsId: string[]
 }
 type CreateQuestionUseCaseResponse = Either<
   null,
@@ -22,12 +24,22 @@ export class CreateQuestionUseCase {
     authorId,
     title,
     content,
+    attachmentsId,
   }: CreateQuestionUseCaseRequest): Promise<CreateQuestionUseCaseResponse> {
     const question = Question.create({
       authorId: new UniqueEntityID(authorId),
       title,
       content,
     })
+
+    const questionAttachments = attachmentsId.map((attachmentsId) => {
+      return QuestionAttachment.create({
+        attachmentId: new UniqueEntityID(attachmentsId),
+        questionId: question.id.toString(),
+      })
+    })
+
+    question.attachments = questionAttachments
     await this.questionsRepository.create(question)
     return right({ question })
   }
--- src/domain/forum/application/use-cases/test/create-question.spec.ts
+++ src/domain/forum/application/use-cases/test/create-question.spec.ts
@@ -1,5 +1,6 @@
 import { InMemoryQuestionsRepository } from 'test/repositories/in-memory-questions-repository'
 import { CreateQuestionUseCase } from '../create-question'
+import { UniqueEntityID } from '@/core/entities/unique-entity-id'
 
 let inMemoryQuestionsRepository: InMemoryQuestionsRepository
 let sut: CreateQuestionUseCase
@@ -14,8 +15,18 @@ describe('Create Question', () => {
       authorId: '1',
       title: 'Que dia é hoje?',
       content: 'Gostaria de saber que dia é hoje.',
+      attachmentsId: ['1', '2'],
     })
     expect(result.isRight()).toBe(true)
     expect(inMemoryQuestionsRepository.items[0]).toEqual(result.value?.question)
+    expect(inMemoryQuestionsRepository.items[0].attachments).toHaveLength(2)
+    expect(inMemoryQuestionsRepository.items[0].attachments).toEqual([
+      expect.objectContaining({
+        attachmentId: new UniqueEntityID('1'),
+      }),
+      expect.objectContaining({
+        attachmentId: new UniqueEntityID('2'),
+      }),
+    ])
   })
 })
